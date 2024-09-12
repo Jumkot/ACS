057d8a888772d4ebabdeff789d0c11064a437dfd
jumkot <jumkot@mail.ru>
2024-09-12 20:47:06 +0700

    Fix gaps and add captions

--- lab_1/sys.sh
+++ lab_1/sys.sh
@@ -14,7 +14,7 @@ echo -e "${L_BLUE}Архитектура ядра Linux:${END} $(uname -m)"
 
 echo -e "\n${PURPLE}\t\tПРОЦЕССОР${END}\n"
 
-echo -e "${PURPLE}Модель:${END} $(grep "model name" /proc/cpuinfo | head -1 | awk -F: '{ print $2 }' | sed 's/^[ \t]*//')"
+echo -e "${PURPLE}Модель:${END} $(grep "model name" /proc/cpuinfo | head -1 | awk -F: '{ print $2 }' | sed 's/^[ \t]*//;s/[ \t][ \t]*/ /g')"
 echo -e "${PURPLE}Частота:${END} $(grep "cpu MHz" /proc/cpuinfo | awk -F: '{ print $2 " MHz" }' | head -n 1 | sed 's/^[ \t]*//')"
 echo -e "${PURPLE}Количество ядер:${END} $(nproc)"
 echo -e "${PURPLE}Потоков на ядро:${END} $(grep "cpu cores" /proc/cpuinfo | head -1 | awk -F: '{print $2}' | sed 's/^[ \t]*//')"
@@ -30,17 +30,19 @@ echo -e "\n${PURPLE}\t    СЕТЕВЫЕ ИНТЕРФЕЙСЫ${END}"
 
 for interface in $(ls /sys/class/net/ | grep -v lo); do
   echo -e "\n${PURPLE}Интерфейс:${END} $interface"
-  ip addr show $interface | grep -E "inet\b" | awk '{print "IP адрес: " $2}'
-  cat /sys/class/net/$interface/address | awk '{print "MAC адрес: " $1}'
+  echo -e "${PURPLE}IP адрес:${END} $(ip addr show $interface | grep -E "inet\b" | awk '{print $2}')"
+  echo -e "${PURPLE}MAC адрес:${END} $(cat /sys/class/net/$interface/address | awk '{print $1}')"
   speed=$(ethtool $interface 2>/dev/null | grep "Speed:" | awk '{print $2}')
   if [ -n "$speed" ]; then
-    echo "Скорость соединения: $speed"
+    echo -e "${PURPLE}Скорость соединения:${END} $speed"
   else
-    echo "Скорость соединения: Не удалось определить"
+    echo -e "${PURPLE}Скорость соединения:${END} Не удалось определить"
   fi
 done
 
 echo -e "\n${L_BLUE}\t     СИСТЕМНЫЕ РАЗДЕЛЫ${END}\n"
+
+echo -e "\n${L_BLUE}      Раздел\t    Точка монтирования\t   Всего      Занято Свободно     В процентах${END}\n"
 
 df -h --output=source,target,size,used,avail,pcent | grep '^/dev/'
 
